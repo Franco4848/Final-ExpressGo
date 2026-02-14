af6a7d623aa860ced0a26afc39b36ef47c4f984c
Franco <francoalvarez4848@gmail.com>
2026-02-14 14:22:16 -0300

    se completo el crud de packages. Funciona: OK

--- src/packages/dto/create-package.dto.ts
+++ src/packages/dto/create-package.dto.ts
@@ -1,1 +1,38 @@
-export class CreatePackageDto {}
+import { IsIn, IsMongoId, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator"
+
+export class CreatePackageDto {
+    @IsString()
+    @IsNotEmpty()
+    trackingId:string;
+
+    @IsString()
+    @IsNotEmpty()
+    address: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    recipientName: string;
+
+    @IsString()
+    @IsIn(['Chico', 'Mediano', 'Grande'])
+    size: string;
+    
+    @IsString()
+    @IsOptional()
+    @IsIn(['PENDIENTE', 'EN_CAMINO', 'ENTREGADO'])
+    status?: string;
+
+    @IsNumber()
+    lat: number;
+
+    @IsNumber()
+    lng: number;
+
+    @IsMongoId() // Valida que sea un ID de Mongo real
+    @IsOptional() // Al crearlo, puede que aún no tenga chofer asignado
+    driverId?: string;
+
+
+
+
+}
--- src/packages/packages.service.ts
+++ src/packages/packages.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
 
 @Injectable()
 export class PackagesService {
-  create(createPackageDto: CreatePackageDto) {
-    return 'This action adds a new package';
+  constructor(private prisma: PrismaService){}
+  
+  async create(createPackageDto: CreatePackageDto) {
+    const newPackage = await this.prisma.package.create({
+      data: createPackageDto
+    })
+    return 'Pedido creado correctamente';
   }
 
   findAll() {
-    return `This action returns all packages`;
+    return this.prisma.package.findMany({
+      include: {driver: true},
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} package`;
+  findOne(id: string) {
+    return this.prisma.package.findUnique({
+      where: {id},
+      include: {driver: true},
+    });
   }
 
-  update(id: number, updatePackageDto: UpdatePackageDto) {
-    return `This action updates a #${id} package`;
+  async update(id: string, updatePackageDto: UpdatePackageDto) {
+    const pakageUpdated = await this.prisma.package.update({
+      where:{id},
+      data: updatePackageDto,
+    })
+    return 'Pedido actualizado correctamente'+ pakageUpdated
   }
 
-  remove(id: number) {
+  async remove(id: string) {
+    const packageDeleted = await this.prisma.package.delete({
+      where: {id},
+    })
     return `This action removes a #${id} package`;
   }
 }

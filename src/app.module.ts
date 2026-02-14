183e4b2355778e0380f8472e3284709a4e961ff8
Franco <francoalvarez4848@gmail.com>
2026-02-14 12:08:35 -0300

    se conecto nest con prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 import { DriversModule } from './drivers/drivers.module';
 import { PackagesModule } from './packages/packages.module';
 
 @Module({
   imports: [DriversModule, PackagesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}

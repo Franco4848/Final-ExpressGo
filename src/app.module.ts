9ed93d3375bd6ed93afb41f9412df9351d06b221
Franco <francoalvarez4848@gmail.com>
2026-02-14 12:06:17 -0300

    se generaron los mudulos de drivers y packages en nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DriversModule } from './drivers/drivers.module';
+import { PackagesModule } from './packages/packages.module';
 
 @Module({
-  imports: [],
+  imports: [DriversModule, PackagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
